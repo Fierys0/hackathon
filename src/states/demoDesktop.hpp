d40660f3beb1b74557b7b07a32a7bb6798c0bae4
Strfrn <stefenshelinten27@gmail.com>
2026-07-17 01:26:16 +0700

    4-menu-fe

--- src/states/demoDesktop.hpp
+++ src/states/demoDesktop.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "../core/os_desktop.hpp"
 #include "fumbo.hpp"
+#include <vector>
 
 // DemoDesktop: Demo state showing OS simulation features
 
@@ -13,6 +14,16 @@ public:
   void DrawDirty() override;
 
 private:
+  struct DecisionLogEntry {
+    std::string timestamp;
+    std::string title;
+    std::string disaster;
+    std::string sector;
+    std::string status;
+    std::string cost;
+    std::string outcome;
+  };
+
   std::shared_ptr<OS::OSDesktop> m_desktop;
 
   // Game state variables for status bar
@@ -21,12 +32,22 @@ private:
   int m_publicTrust;
   std::string m_cityStatus;
 
+  std::vector<DecisionLogEntry> m_decisionLog;
+  int m_selectedDecisionLogEntry;
+  int m_decisionLogScroll;
+
   // Setup helpers (called from Init)
   void SetupDesktopIcons();
   void SetupStartMenu();
   void SetupContextMenu();
   void SetupSystemTray();
   void DrawStatusBar();
+  void AddDecisionLogEntry(const std::string &title,
+                           const std::string &disaster,
+                           const std::string &sector,
+                           const std::string &status,
+                           const std::string &cost,
+                           const std::string &outcome);
 
   // Window content builders
   static void DrawTerminalContent(Rectangle area);
@@ -36,6 +57,6 @@ private:
   static void DrawAboutContent(Rectangle area);
   static void DrawThreatCenterContent(Rectangle area);
   static void DrawCommsContent(Rectangle area);
-  static void DrawMitigationHub(Rectangle area);
-  static void DrawAnalysisCenter(Rectangle area);
+  void DrawMitigationHub(Rectangle area);
+  void DrawDecisionLog(Rectangle area);
 };
